33c0f77eb465fb77a54efd7032bfcb98f7711e8a
Vaidehi54 <Vaidehi_5489@yahoo.com>
2018-02-04 13:05:48 +0000

    First commit added

---
+++ src/DayOne.java
@@ -0,0 +1,11 @@
+public class DayOne {
+    public static void main (String[] args){
+        String greeting = "Hi Hello How are you?";
+        System.out.print(greeting);
+        System.out.print( "There");
+       // System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.print("World");
+    }
+}
+
